65faf89aa6741726cfbe5f161fc6f84242698844
ediharianto1974 <g-11282159@moe-dl.edu.my>
2026-04-14 14:05:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -569,7 +569,6 @@ async function resetSemuaData() {
             alert("❌ Gagal reset.");
         }
     }
-  }
 
 // --- ANALISIS 3: PASUKAN MENGIKUT KATEGORI UMUR (BAHARU) ---
     analisisPemenangPasukanKategoriUmur() {
